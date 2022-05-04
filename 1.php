cfc36c2b420a420f410927e29150ec656fadedae
RafaelFassa <rafael.teixeira0912@gmail.com>
2022-05-04 08:54:43 -0300

    gostosa

---
+++ 1.php
@@ -0,0 +1,39 @@
+<?php
+$nm = $_GET['nm'];
+
+switch ($nm){
+
+   case 1: 
+      echo "descançar";
+      break;
+
+   case 2: 
+       echo "Va estudar";
+      break;
+
+   case 3: 
+       echo "Va estudar";
+      break;
+
+    case 4: 
+        echo "Va estudar";
+       break;
+
+    case 5: 
+        echo "Va estudar";
+       break;
+
+    case 6: 
+       echo "Va estudar";
+       break;
+
+    case 7: 
+       echo "descançar";
+       break;
+
+   default:
+      echo "Digite um numero de 1 a 7";
+     break;
+   }
+?>
+       
